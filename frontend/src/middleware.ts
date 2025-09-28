7d94bbb678eff33b5672b90c9f4270e7edc3e0b9
huukha04 <kha.huynhhuu1810@gmail.com>
2025-09-29 00:57:40 +0700

    Update

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,4 +1,4 @@
-// export { auth as middleware } from '@/helper/auth';
+export { auth as middleware } from '@/helper/auth';
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
